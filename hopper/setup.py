131df3540208ebdc489b744fafa034d2f50e72dc
Pragaash <pragaash.io@gmail.com>
2024-07-18 23:26:09 -0700

    [FA3] Enable support for ninja build.

--- hopper/setup.py
+++ hopper/setup.py
@@ -254,6 +254,30 @@ class CachedWheelsCommand(_bdist_wheel):
             # If the wheel could not be downloaded, build from source
             super().run()
 
+
+class NinjaBuildExtension(BuildExtension):
+
+    def __init__(self, *args, **kwargs) -> None:
+
+        # do not override env MAX_JOBS if already exists
+        if not os.environ.get("MAX_JOBS"):
+
+            import psutil
+
+            # calculate the maximum allowed NUM_JOBS based on cores
+            max_num_jobs_cores = max(1, os.cpu_count() // 2)
+
+            # calculate the maximum allowed NUM_JOBS based on free memory
+            free_memory_gb = psutil.virtual_memory().available / (1024 ** 3)  # free memory in GB
+            max_num_jobs_memory = int(free_memory_gb / 9)  # each JOB peak memory cost is ~8-9GB when threads = 4
+
+            # pick lower value of jobs based on cores vs memory metric to minimize oom and swap usage during compilation
+            max_jobs = max(1, min(max_num_jobs_cores, max_num_jobs_memory))
+            os.environ["MAX_JOBS"] = str(max_jobs)
+
+        super().__init__(*args, **kwargs)
+
+
 setup(
     name=PACKAGE_NAME,
     version=get_package_version(),
@@ -277,11 +301,11 @@ setup(
         "Operating System :: Unix",
     ],
     ext_modules=ext_modules,
-    cmdclass={"bdist_wheel": CachedWheelsCommand, "build_ext": BuildExtension}
-    if ext_modules
-    else {
-        "bdist_wheel": CachedWheelsCommand,
-    },
+    cmdclass=(
+        {"bdist_wheel": CachedWheelsCommand, "build_ext": NinjaBuildExtension}
+        if ext_modules
+        else {"bdist_wheel": CachedWheelsCommand}
+    ),
     python_requires=">=3.8",
     install_requires=[
         "torch",
